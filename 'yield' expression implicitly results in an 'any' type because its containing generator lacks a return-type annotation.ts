e402c0f5913193be31589c182af4a8d55d768e9d
Charles-ShiZ <62864818+Charles-ShiZ@users.noreply.github.com>
2021-12-13 19:30:48 +0800

    Create 'yield' expression implicitly results in an 'any' type because its containing generator lacks a return-type annotation.ts

---
+++ 'yield' expression implicitly results in an 'any' type because its containing generator lacks a return-type annotation.ts
@@ -0,0 +1,10 @@
+'yield' expression implicitly results in an 'any' type because its containing generator lacks a return-type annotation.ts
+
+解决：
+```js
+  const brand = yield select((state: TRootState) => state.common.siteBrand)
+```
+改为：
+```js
+  const brand = yield select((state: TRootState) => state.common.siteBrand)
+```
